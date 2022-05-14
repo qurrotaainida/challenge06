56a21e341effad6fac44df6ae0196549bbcf5081
qurrotaainida <qurrotaainida250701@gmail.com>
2022-05-14 11:54:38 +0700

    first commit

---
+++ src/Components/Challenge/store/userSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+import jwtDecode from "jwt-decode";
+
+let initialState = null;
+
+try {
+	const token = localStorage.getItem("carShopAccessToken");
+	const userData = jwtDecode(token);
+	initialState = userData;
+} catch {}
+
+const userSlice = createSlice({
+	name: "user",
+	initialState: {
+		data: initialState,
+	},
+	reducers: {
+		addUser: (state, action) => {
+			state.data = { ...action.payload.userData };
+		},
+
+		removeUser: (state) => {
+			state.data = null;
+		},
+	},
+});
+
+export default userSlice;
---
+++ src/Components/HOC/AdminRoute.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+
+const AdminRoute = () => {
+	const user = useSelector((store) => store.user.data);
+
+	
+	if (user.isAdmin === true) {
+		return <Navigate to="/admin" />;
+	} 
+	else {
+		return <Outlet />;
+	}
+};
+
+export default AdminRoute;
